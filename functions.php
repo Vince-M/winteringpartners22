eee5bad5acc038271fe96426f02964ad6633e538
Vince Mendella <vmendella@gmail.com>
2022-08-24 15:33:02 -0500

    added space to different functions

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function wpg_files() {
-  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=League+Spartan:wght@300;400;700&display=swap"');
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=League+Spartan:wght@300;400;700&display=swap');
   wp_enqueue_style('wpg_main_styles', get_theme_file_uri('main.css'));
   wp_enqueue_style('wpg_styles', get_theme_file_uri('style.css'));
   wp_enqueue_script('wpg_script_js', get_stylesheet_directory_uri() . '/dist/script.js', NULL, '1.0', false);
